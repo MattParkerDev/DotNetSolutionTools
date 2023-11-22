83c5676f1d26abd304a7e371a02c3abe75663b70
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-11-22 13:39:03 +1000

    Also delete node_modules

--- DotNetSolutionTools.Core/CleanFolder.cs
+++ DotNetSolutionTools.Core/CleanFolder.cs
@@ -5,6 +5,15 @@ public static class CleanFolder
 {
     public static void DeleteFolderWithOnlyBinAndObjSubFolders(string folderPath)
     {
+        var nodeModulesFolders = Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories)
+            .Where(x => x.EndsWith(Path.DirectorySeparatorChar + "node_modules"))
+            .ToList();
+        
+        foreach (var folder in nodeModulesFolders)
+        {
+            Directory.Delete(folder, true);
+        }
+        
         var binAndObjFolders = Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories)
             .Where(x => x.EndsWith(Path.DirectorySeparatorChar + "bin") || x.EndsWith(Path.DirectorySeparatorChar + "obj"))
             .ToList();
