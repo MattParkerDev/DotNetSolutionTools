c7e57d9ff26bdbf55cac2be83784bce4f097d4fb
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-11-29 14:09:40 +1000

    Rider plugin

---
+++ DotNetSolutionTools.RiderPlugin/src/dotnet/ReSharperPlugin.DotNetSolutionTools.RiderPlugin.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.DotNetSolutionTools.RiderPlugin.Tests
+{
+    [ZoneDefinition]
+    public class DotNetSolutionTools.RiderPluginTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IDotNetSolutionTools.RiderPluginZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<DotNetSolutionTools.RiderPluginTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class DotNetSolutionTools.RiderPluginTestsAssembly : ExtensionTestEnvironmentAssembly<DotNetSolutionTools.RiderPluginTestEnvironmentZone> { }
+}
