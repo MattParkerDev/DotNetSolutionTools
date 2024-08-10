2578cd9bd071f2fccb8d0648158e6fc30410fdd7
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2024-08-11 00:56:21 +1000

    asdf

---
+++ PackageVersionArchTest/Helper.cs
@@ -0,0 +1,37 @@
+﻿using DotNetSolutionTools.Core;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Locator;
+
+namespace PackageVersionArchTest;
+
+public static class PackageArchTest
+{
+    public static bool AllNugetVersionsAreConsistent(string solutionFileName)
+    {
+        var instance = MSBuildLocator
+            .QueryVisualStudioInstances()
+            .OrderByDescending(instance => instance.Version)
+            .First();
+        MSBuildLocator.RegisterInstance(instance);
+
+        var currentDirectory = Directory.GetCurrentDirectory();
+        while (!File.Exists(Path.Combine(currentDirectory, solutionFileName)))
+        {
+            currentDirectory = Directory.GetParent(currentDirectory)?.FullName;
+            if (currentDirectory is null)
+            {
+                throw new FileNotFoundException("Solution file not found.");
+            }
+        }
+        var solutionFilePath = Path.Combine(currentDirectory, solutionFileName);
+
+        var result = PackageVersionConsistency.FindInconsistentNugetPackageVersions(solutionFilePath);
+
+        if (result.Count > 0)
+        {
+            throw new Exception($"Inconsistent package versions found: {string.Join(", ", result)}");
+        }
+
+        return true;
+    }
+}
---
+++ PackageVersionArchTest/readme.md
@@ -0,0 +1,3 @@
+# PackageVersionArchTest
+
+An architecture test to ensure your NuGet packages are all on consistent versions!
