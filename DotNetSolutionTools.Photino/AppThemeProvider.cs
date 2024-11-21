7720c6f8a59260c602a9c4aae572ea3af04a9fc6
Matt Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2024-11-21 14:55:31 +1000

    asdf

---
+++ DotNetSolutionTools.Photino/AppThemeProvider.cs
@@ -0,0 +1,15 @@
+﻿using MudBlazor;
+
+namespace DotNetSolutionTools.Photino;
+
+public static class AppThemeProvider
+{
+    public static MudTheme GetTheme()
+    {
+        var theme = new MudTheme();
+        theme.Typography.H5.FontSize = "1.4rem";
+        theme.Typography.H5.FontWeight = 500;
+        theme.Typography.H6.FontSize = "1.1rem";
+        return theme;
+    }
+}
---
+++ DotNetSolutionTools.Photino/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using Photino.Blazor;
+
+namespace DotNetSolutionTools.Photino;
+
+public class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        appBuilder.Services.AddLogging();
+        appBuilder.Services.AddMudServices();
+
+        appBuilder.RootComponents.Add<App>("app");
+
+        var app = appBuilder.Build();
+
+        app.MainWindow.SetSize(1400, 800)
+            .SetDevToolsEnabled(true)
+            .SetLogVerbosity(0)
+            //.SetIconFile("favicon.ico")
+            .SetTitle("DotNetSolutionTools.Photino");
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}
