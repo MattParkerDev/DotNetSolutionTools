0daa3f9304616f4f8fb8daa616f5db3182f61786
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-05-18 14:15:01 +1000

    Update local-publish.bat

--- local-publish.bat
+++ local-publish.bat
@@ -1,3 +1,3 @@
 @echo on
-dotnet build --configuration release
-dotnet publish ./DotNetSolutionTools.App/DotNetSolutionTools.App.csproj -c release -o output -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true --self-contained false
+dotnet build -c Release
+dotnet publish ./src/DotNetSolutionTools.App/DotNetSolutionTools.App.csproj -c Release -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true --self-contained false
